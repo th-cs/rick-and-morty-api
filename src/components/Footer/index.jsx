6de92713b4bc1f525295535a4481f97bf213da48
th-cs <202718515+th-cs@users.noreply.github.com>
2026-04-11 12:49:44 -0300

    feat: Add Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+
+export const Footer = () => {
+
+	return (
+		<footer>
+			<p>
+				Desenvolvido por <span>TH-CS</span>
+			</p>
+		</footer>
+	);
+}
+
+export default Footer;

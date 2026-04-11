336577d105b7d978a5047ed7f3a99a086f5816a8
th-cs <202718515+th-cs@users.noreply.github.com>
2026-04-11 13:30:52 -0300

    refactor: Add intro and content div tags

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -3,24 +3,36 @@ import "./style.css";
 export const Main = () => {
 	return(
 		<main>
-			<img
-				src="/src/assets/images/rick-img.png"
-				alt="Imagem do Rick"/>
-			<div className="main-content">
+			<div className="main-intro">
 				<img
-					src="/src/assets/icons/rick-and-morty-logo.svg"
-					width="430px"
-					height="126px"
-					alt="Logo de Rick and Morty"/>
-				<h1>Bem-vindos ao <span>consumo de API</span> do Rick and Morty</h1>
-				<p>Aqui você encontra informações sobre os personagens de Rick and Morty.</p>
-				<div className="main-container-btn">
-					<a
-						href="#"
-						className="btn-primary">Veja agora</a>
-					<a
-						href="#"
-						className="btn-secondary">Saiba mais</a>
+					src="/src/assets/images/rick-img.png"
+					alt="Imagem do Rick"/>
+				<div className="main-intro-text">
+					<img
+						src="/src/assets/icons/rick-and-morty-logo.svg"
+						width="430px"
+						height="126px"
+						alt="Logo de Rick and Morty"/>
+					<h1>
+						Bem-vindos ao <span>consumo de API</span> do Rick and Morty
+					</h1>
+					<p>Aqui você encontra informações sobre os personagens de Rick and Morty.</p>
+					<div className="main-container-btn">
+						<a
+							href="#"
+							className="btn-primary">Veja agora</a>
+						<a
+							href="#"
+							className="btn-secondary">Saiba mais</a>
+					</div>
+				</div>
+			</div>
+			<div className="main-content">
+				<h2>
+					Nossos <span>Personagens</span>
+				</h2>
+				<div className="main-cards">
+
 				</div>
 			</div>
 		</main>
